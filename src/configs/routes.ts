961e19f853d86563635983bcdfdb62f0d8cb4a36
NTKH <hn26677@gmail.com>
2024-12-12 10:39:56 +0700

    feat:route

---
+++ src/configs/routes.ts
@@ -0,0 +1,2 @@
+export const publicRoutes = ['/login']
+export const protectedRoutes = ['/', '/bill', '/cinema', '/movie', '/user', '']
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,22 @@
+import { protectedRoutes, publicRoutes } from '@/configs/routes'
+import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
+export default async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname
+    const isPublicRoute = publicRoutes.includes(path)
+    const isProtectedRoute = protectedRoutes.includes(path)
+    const token = cookies().get('_session')?.value
+    if (isPublicRoute && token) {
+        return NextResponse.redirect(new URL('/', req.nextUrl))
+    }
+    if (isProtectedRoute && !token) {
+        const loginUrl = new URL('/login', req.nextUrl)
+
+        return NextResponse.redirect(loginUrl)
+    }
     return NextResponse.next()
+}
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
